62f81b37c3f2864e16729b190de0f33c8ef58804
norah-aln <zylayo@gmail.com>
2026-01-06 21:23:28 +0000

    Task 2: Implement User endpoints (POST, GET, PUT)

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -9,96 +9,32 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    def __init__(self):
-        """Initialize repositories for each entity."""
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
-
-    # User methods
-    def create_user(self, user_data):
-        """
-        Create a new user.
-        
-        Args:
-            user_data: Dictionary containing user information
-            
-        Returns:
-            Created User object
-        """
+    ` def create_user(self, data):
+        """Create user."""
         user = User(
-            first_name=user_data['first_name'],
-            last_name=user_data['last_name'],
-            email=user_data['email'],
-            password=user_data['password'],
-            is_admin=user_data.get('is_admin', False)
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            password=data['password']
         )
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
-        """
-        Retrieve a user by ID.
-        
-        Args:
-            user_id: Unique identifier of the user
-            
-        Returns:
-            User object or None if not found
-        """
+        """Get user by ID."""
         return self.user_repo.get(user_id)
 
     def get_all_users(self):
-        """
-        Retrieve all users.
-        
-        Returns:
-            List of all User objects
-        """
+        """Get all users."""
         return self.user_repo.get_all()
 
-    def update_user(self, user_id, user_data):
-        """
-        Update user information.
-        
-        Args:
-            user_id: Unique identifier of the user
-            user_data: Dictionary containing updated user information
-            
-        Returns:
-            Updated User object or None if not found
-        """
+    def update_user(self, user_id, data):
+        """Update user."""
         user = self.user_repo.get(user_id)
         if user:
-            user.update(user_data)
+            user.update(data)
         return user
 
     def get_user_by_email(self, email):
-        """
-        Retrieve a user by email.
-        
-        Args:
-            email: Email address of the user
-            
-        Returns:
-            User object or None if not found
-        """
+        """Get user by email."""
         return self.user_repo.get_by_attribute('email', email)
-
-    # Placeholder methods for other entities (will be implemented later)
-    def create_place(self, place_data):
-        """Placeholder for place creation."""
-        pass
-
-    def get_place(self, place_id):
-        """Placeholder for fetching a place by ID."""
-        pass
-
-    def create_amenity(self, amenity_data):
-        """Placeholder for amenity creation."""
-        pass
-
-    def create_review(self, review_data):
-        """Placeholder for review creation."""
-        pass 
