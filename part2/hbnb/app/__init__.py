aca8d6c83d2018ebd90f559b9ef8d23e1f1a139c
norah-aln <zylayo@gmail.com>
2026-01-06 21:32:58 +0000

    Fix Task 2

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,14 +2,12 @@
 from flask import Flask
 from flask_restx import Api
 
+
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
-
-    # Placeholder for API namespaces (endpoints will be added later)
-     from app.api.v1.users import api as users_ns
-     api.add_namespace(users_ns, path='/api/v1/users')
-
-    # Additional namespaces for places, reviews, and amenities will be added later
-
+    api = Api(app, version='1.0', title='HBnB API', doc='/api/v1/docs')
+    
+    from app.api.v1.users import api as users_ns
+    api.add_namespace(users_ns, path='/api/v1/users')
+    
     return app
