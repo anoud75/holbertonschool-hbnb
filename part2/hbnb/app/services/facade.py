6d3a50ec98bf8955a3b4ee4412c15c8fde982573
agalotaibi <hopes.gmo@gmail.com>
2026-01-08 14:50:27 +0000

    updating the project

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,44 +1,145 @@
-#!/usr/bin/python3
-"""Facade pattern."""
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
-
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
 
 class HBnBFacade:
-    """Facade class."""
-
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    def create_user(self, data):
-        """Create user."""
-        user = User(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            email=data['email'],
-            password=data['password']
-        )
+    # ---- User ----
+    def create_user(self, user_data):
+        user = User(**user_data)
         self.user_repo.add(user)
         return user
-
+    
     def get_user(self, user_id):
-        """Get user by ID."""
         return self.user_repo.get(user_id)
-
+    
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+    
     def get_all_users(self):
-        """Get all users."""
         return self.user_repo.get_all()
-
-    def update_user(self, user_id, data):
-        """Update user."""
+    
+    def update_user(self, user_id, user_data):
         user = self.user_repo.get(user_id)
         if user:
-            user.update(data)
+            for key, value in user_data.items():
+                if key != 'id' and key != 'email':
+                     setattr(user, key, value)
         return user
+    
+    # ---- Amenity ----
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
-    def get_user_by_email(self, email):
-        """Get user by email."""
-        return self.user_repo.get_by_attribute('email', email)
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if amenity:
+            amenity.update(amenity_data)
+        return amenity
+
+    # ---- Place ----
+    def create_place(self, place_data):
+        owner_id = place_data.get('owner_id')
+        owner = self.get_user(owner_id)
+        if not owner:
+            raise ValueError("Owner not found")
+        place = Place(
+            title=place_data['title'],
+            description=place_data.get('description', ''),
+            price=place_data['price'],
+            latitude=place_data['latitude'],
+            longitude=place_data['longitude'],
+            owner=owner
+        )
+        amenity_ids = place_data.get('amenities', [])
+        for amenity_id in amenity_ids:
+            amenity = self.get_amenity(amenity_id)
+            if amenity:
+                place.add_amenity(amenity)
+        self.place_repo.add(place)
+        return place
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.place_repo.get(place_id)
+        if place:
+            for key, value in place_data.items():
+                if key != 'id' and key != 'owner_id' and key != 'amenities':
+                    setattr(place, key, value)
+            if 'amenities' in place_data:
+                place.amenities = []
+                for amenity_id in place_data['amenities']:
+                    amenity = self.get_amenity(amenity_id)
+                    if amenity:
+                        place.add_amenity(amenity)
+        return place
+    
+
+    # --- Review ---
+
+    def create_review(self, review_data):
+        user = self.get_user(review_data['user_id'])
+        place = self.get_place(review_data['place_id'])
+
+        if not user:
+            raise ValueError("User not found")
+        
+        if not place:
+            raise ValueError("Place not found")
+        
+        review = Review(
+            text=review_data['text'],
+            rating=review_data['rating'],
+            place=place,
+            user=user
+        )
+
+        self.review_repo.add(review)
+        place.add_review(review)
+
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.amenity_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        place = self.get_place(place_id)
+        if not place:
+            return None
+        return place.reviews
+    
+
+    def update_review(self, review_id, review_data):
+        review = self.review_repo.get(review_id)
+        if review:
+            review.update(review_data)
+        return review
+
+
+    def delete_review(self, review_id):
+        review = self.review_repo.get(review_id)
+        if review:
+            self.review_repo.delete(review_id)
