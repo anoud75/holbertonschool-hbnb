21c427fae27a13abf32a6222e4b16a80760fa382
norah-aln <zylayo@gmail.com>
2026-01-21 13:28:47 +0000

    fix task

--- part3/hbnb/app/models/__init__.py
+++ part3/hbnb/app/models/__init__.py
@@ -1,1 +1,11 @@
- 
+"""
+Models package initialization.
+Exports all model classes for easier imports throughout the application.
+"""
+
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
+
+__all__ = ['User', 'Place', 'Review', 'Amenity'] 
